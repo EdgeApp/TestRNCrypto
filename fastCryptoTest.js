15f558fb2103b348d2d09a9370fa5b2bc5de35c1
Paul Puey <paul@edgesecure.co>
2017-12-19 19:44:04 -0800

    Add secp256k1.publicKeyCreate tests

--- fastCryptoTest.js
+++ fastCryptoTest.js
@@ -19,6 +19,7 @@ export default class TestRNCrypto extends Component {
 
   async runtest () {
     testScrypt(this.log)
+    testPublicKeyCreate(this.log)
   }
 
   log = (s) => {
@@ -105,4 +106,68 @@ async function testScrypt (log) {
     }
   }
   log('PASSED scrypt')
+}
+
+async function testPublicKeyCreate (log) {
+  let privateKeyHex = '7bd083869bd02e57786d119a81d406ac1524a765470acee90834c60f841e6236'
+  let publicKeyHex = '02fce1b65c7470270fb89e5aa1dda253406b3370b948eae8a2a3539c4f25894283'
+  let result
+  let time
+  privateKeyHex = '7bd083869bd02e57786d119a81d406ac1524a765470acee90834c60f841e6236'
+  publicKeyHex = '02fce1b65c7470270fb89e5aa1dda253406b3370b948eae8a2a3539c4f25894283'
+  time = Date.now()
+  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  time = Date.now() - time
+  if (result !== publicKeyHex) {
+    log('FAILED secp256k1.publicKeyCreate 1')
+    return
+  }
+  log('PASSED secp256k1.publicKeyCreate 1. Time:' + time.toString())
+
+  privateKeyHex = 'e8f32e723decf4051aefac8e2c93c9c5b214313817cdb01a1494b917c8436b35'
+  publicKeyHex = '0339a36013301597daef41fbe593a02cc513d0b55527ec2df1050e2e8ff49c85c2'
+  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  if (result !== publicKeyHex) {
+    log('FAILED secp256k1.publicKeyCreate 2')
+    return
+  }
+  log('PASSED secp256k1.publicKeyCreate 2')
+
+  privateKeyHex = 'b7aaa6c8f5b4013e7b1755ae877b9fad530e9b2e86f68cec7420a6cea38e81b0'
+  publicKeyHex = '031cd583c3ffb87477c539012859195f821e33b16435e8f3da0b40247056158618'
+  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  if (result !== publicKeyHex) {
+    log('FAILED secp256k1.publicKeyCreate 3')
+    return
+  }
+  log('PASSED secp256k1.publicKeyCreate 3')
+
+  privateKeyHex = '00000055378cf5fafb56c711c674143f9b0ee82ab0ba2924f19b64f5ae7cdbfd'
+  publicKeyHex = '02b3e3e297165289611a2387e8089fcaf099926e4d31fdddb50c0ae0dfa36c97e6'
+  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  if (result !== publicKeyHex) {
+    log('FAILED secp256k1.publicKeyCreate 4')
+    return
+  }
+  log('PASSED secp256k1.publicKeyCreate 4')
+
+  privateKeyHex = 'f1d52574bfbc006ef23c6f513fb1cfd76fe8f6fedd842ac9ca62f1722a5c24c7'
+  publicKeyHex = '02d87551d78c8bf6ea5bfacf9d547e864833f41b337a8de2cac89c891e9b98bff9'
+  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  if (result !== publicKeyHex) {
+    log('FAILED secp256k1.publicKeyCreate 5')
+    return
+  }
+  log('PASSED secp256k1.publicKeyCreate 5')
+
+  privateKeyHex = '8eb1015391987e1e83c95ffe3352b61387dc2a78b6da813c8e35231f3a1ebcb7'
+  publicKeyHex = '028296aa04520743938d2bedd5999294e0882fc341075918e0e8df3e74ba9916ef'
+  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  if (result !== publicKeyHex) {
+    log('FAILED secp256k1.publicKeyCreate 6')
+    return
+  }
+  log('PASSED secp256k1.publicKeyCreate 6')
+
+  log('PASSED secp256k1.publicKeyCreate ALL')
 }
