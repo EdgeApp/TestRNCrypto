51bd445eb83950a207fa5b9f2e03dbc6027927a6
Paul Puey <paul@edgesecure.co>
2017-12-27 18:29:00 -0800

    Fix tests to use Buffers for secp256k1 crypto routines

--- fastCryptoTest.js
+++ fastCryptoTest.js
@@ -8,6 +8,7 @@ import crypto from 'react-native-fast-crypto'
 
 import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+const Buffer = require('buffer/').Buffer
 
 export default class TestRNCrypto extends Component {
   constructor (props) {
@@ -45,14 +46,14 @@ export default class TestRNCrypto extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
     backgroundColor: '#F5FCFF'
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 12,
     textAlign: 'center',
-    margin: 10
+    margin: 5
   }
   // instructions: {
   //   textAlign: 'center',
@@ -115,61 +116,92 @@ async function testPublicKeyCreate (log) {
   let publicKeyHex
   let result
   let time
+  let testName
+  let privateKey
+
+  testName = 'secp256k1.publicKeyCreate 1'
   privateKeyHex = '7bd083869bd02e57786d119a81d406ac1524a765470acee90834c60f841e6236'
   publicKeyHex = '02fce1b65c7470270fb89e5aa1dda253406b3370b948eae8a2a3539c4f25894283'
   time = Date.now()
-  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  privateKey = Buffer.from(privateKeyHex, 'hex')
+  result = await crypto.secp256k1.publicKeyCreate(privateKey, true)
+  result = result.toString('hex')
   time = Date.now() - time
   if (result !== publicKeyHex) {
-    log('FAILED secp256k1.publicKeyCreate 1')
+    log('FAILED ' + testName)
     return
   }
-  log('PASSED secp256k1.publicKeyCreate 1. Time:' + time.toString())
+  log('PASSED ' + testName + ' Time:' + time.toString())
 
+  testName = 'secp256k1.publicKeyCreate 2'
   privateKeyHex = 'e8f32e723decf4051aefac8e2c93c9c5b214313817cdb01a1494b917c8436b35'
   publicKeyHex = '0339a36013301597daef41fbe593a02cc513d0b55527ec2df1050e2e8ff49c85c2'
-  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  time = Date.now()
+  privateKey = Buffer.from(privateKeyHex, 'hex')
+  result = await crypto.secp256k1.publicKeyCreate(privateKey, true)
+  result = result.toString('hex')
+  time = Date.now() - time
   if (result !== publicKeyHex) {
-    log('FAILED secp256k1.publicKeyCreate 2')
+    log('FAILED ' + testName)
     return
   }
-  log('PASSED secp256k1.publicKeyCreate 2')
+  log('PASSED ' + testName + ' Time:' + time.toString())
 
+  testName = 'secp256k1.publicKeyCreate 3'
   privateKeyHex = 'b7aaa6c8f5b4013e7b1755ae877b9fad530e9b2e86f68cec7420a6cea38e81b0'
   publicKeyHex = '031cd583c3ffb87477c539012859195f821e33b16435e8f3da0b40247056158618'
-  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  time = Date.now()
+  privateKey = Buffer.from(privateKeyHex, 'hex')
+  result = await crypto.secp256k1.publicKeyCreate(privateKey, true)
+  result = result.toString('hex')
+  time = Date.now() - time
   if (result !== publicKeyHex) {
-    log('FAILED secp256k1.publicKeyCreate 3')
+    log('FAILED ' + testName)
     return
   }
-  log('PASSED secp256k1.publicKeyCreate 3')
+  log('PASSED ' + testName + ' Time:' + time.toString())
 
+  testName = 'secp256k1.publicKeyCreate 4'
   privateKeyHex = '00000055378cf5fafb56c711c674143f9b0ee82ab0ba2924f19b64f5ae7cdbfd'
   publicKeyHex = '02b3e3e297165289611a2387e8089fcaf099926e4d31fdddb50c0ae0dfa36c97e6'
-  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  time = Date.now()
+  privateKey = Buffer.from(privateKeyHex, 'hex')
+  result = await crypto.secp256k1.publicKeyCreate(privateKey, true)
+  result = result.toString('hex')
+  time = Date.now() - time
   if (result !== publicKeyHex) {
-    log('FAILED secp256k1.publicKeyCreate 4')
+    log('FAILED ' + testName)
     return
   }
-  log('PASSED secp256k1.publicKeyCreate 4')
+  log('PASSED ' + testName + ' Time:' + time.toString())
 
+  testName = 'secp256k1.publicKeyCreate 5'
   privateKeyHex = 'f1d52574bfbc006ef23c6f513fb1cfd76fe8f6fedd842ac9ca62f1722a5c24c7'
   publicKeyHex = '02d87551d78c8bf6ea5bfacf9d547e864833f41b337a8de2cac89c891e9b98bff9'
-  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  time = Date.now()
+  privateKey = Buffer.from(privateKeyHex, 'hex')
+  result = await crypto.secp256k1.publicKeyCreate(privateKey, true)
+  result = result.toString('hex')
+  time = Date.now() - time
   if (result !== publicKeyHex) {
-    log('FAILED secp256k1.publicKeyCreate 5')
+    log('FAILED ' + testName)
     return
   }
-  log('PASSED secp256k1.publicKeyCreate 5')
+  log('PASSED ' + testName + ' Time:' + time.toString())
 
+  testName = 'secp256k1.publicKeyCreate 6'
   privateKeyHex = '8eb1015391987e1e83c95ffe3352b61387dc2a78b6da813c8e35231f3a1ebcb7'
   publicKeyHex = '028296aa04520743938d2bedd5999294e0882fc341075918e0e8df3e74ba9916ef'
-  result = await crypto.secp256k1.publicKeyCreate(privateKeyHex, true)
+  time = Date.now()
+  privateKey = Buffer.from(privateKeyHex, 'hex')
+  result = await crypto.secp256k1.publicKeyCreate(privateKey, true)
+  result = result.toString('hex')
+  time = Date.now() - time
   if (result !== publicKeyHex) {
-    log('FAILED secp256k1.publicKeyCreate 6')
+    log('FAILED ' + testName)
     return
   }
-  log('PASSED secp256k1.publicKeyCreate 6')
+  log('PASSED ' + testName + ' Time:' + time.toString())
 
   log('PASSED secp256k1.publicKeyCreate ALL')
 }
@@ -177,56 +209,78 @@ async function testPublicKeyCreate (log) {
 async function testPrivateKeyTweakAdd (log) {
   let privateKeyHex
   let tweakHex
+  let privateKeyTweakAddHex
+  let privateKey
+  let tweak
   let privateKeyTweakAdd
   let result
   let time
+  let testName
+
+  testName = 'secp256k1.privateKeyTweakAdd 1'
   privateKeyHex = '7bd083869bd02e57786d119a81d406ac1524a765470acee90834c60f841e6236'
   tweakHex = 'e65bfe78107a0d9b60086753695f7e0b55ae4dd1248f2779803582c27a946ea1'
-  privateKeyTweakAdd = '622c81feac4a3bf2d87578edeb3384b8b024184fbc515626c897ea452e7c8f96'
+  privateKeyTweakAddHex = '622c81feac4a3bf2d87578edeb3384b8b024184fbc515626c897ea452e7c8f96'
   time = Date.now()
-  result = await crypto.secp256k1.privateKeyTweakAdd(privateKeyHex, tweakHex)
+  privateKey = Buffer.from(privateKeyHex, 'hex')
+  tweak = Buffer.from(tweakHex, 'hex')
+  result = await crypto.secp256k1.privateKeyTweakAdd(privateKey, tweak)
+  result = result.toString('hex')
   time = Date.now() - time
-  if (result !== privateKeyTweakAdd) {
-    log('FAILED secp256k1.privateKeyTweakAdd 1')
+  if (result !== privateKeyTweakAddHex) {
+    log('FAILED ' + testName)
     return
   }
-  log('PASSED secp256k1.privateKeyTweakAdd 1. Time:' + time.toString())
+  log('PASSED ' + testName + '. Time:' + time.toString())
 
+
+  testName = 'secp256k1.privateKeyTweakAdd 2'
   privateKeyHex = 'f1c7c871a54a804afe328b4c83a1c33b8e5ff48f5087273f04efa83b247d6a2d'
   tweakHex = 'c9b5714c12f44f0df4caf76a557670dfeb4310bd7db193b6370b5ad5ccde8337'
-  privateKeyTweakAdd = 'bb7d39bdb83ecf58f2fd82b6d918341cbef428661ef01ab97c28a4842125ac23'
+  privateKeyTweakAddHex = 'bb7d39bdb83ecf58f2fd82b6d918341cbef428661ef01ab97c28a4842125ac23'
   time = Date.now()
-  result = await crypto.secp256k1.privateKeyTweakAdd(privateKeyHex, tweakHex)
+  privateKey = Buffer.from(privateKeyHex, 'hex')
+  tweak = Buffer.from(tweakHex, 'hex')
+  result = await crypto.secp256k1.privateKeyTweakAdd(privateKey, tweak)
+  result = result.toString('hex')
   time = Date.now() - time
-  if (result !== privateKeyTweakAdd) {
-    log('FAILED secp256k1.privateKeyTweakAdd 2')
+  if (result !== privateKeyTweakAddHex) {
+    log('FAILED ' + testName)
     return
   }
-  log('PASSED secp256k1.privateKeyTweakAdd 2. Time:' + time.toString())
+  log('PASSED ' + testName + '. Time:' + time.toString())
 
+  testName = 'secp256k1.privateKeyTweakAdd 3'
   privateKeyHex = '704addf544a06e5ee4bea37098463c23613da32020d604506da8c0518e1da4b7'
   tweakHex = '817cea7c60aa11ec1973e7dbeb5b87182d22516f2fb122ee9746e7e9965fc576'
-  privateKeyTweakAdd = 'f1c7c871a54a804afe328b4c83a1c33b8e5ff48f5087273f04efa83b247d6a2d'
+  privateKeyTweakAddHex = 'f1c7c871a54a804afe328b4c83a1c33b8e5ff48f5087273f04efa83b247d6a2d'
   time = Date.now()
-  result = await crypto.secp256k1.privateKeyTweakAdd(privateKeyHex, tweakHex)
+  privateKey = Buffer.from(privateKeyHex, 'hex')
+  tweak = Buffer.from(tweakHex, 'hex')
+  result = await crypto.secp256k1.privateKeyTweakAdd(privateKey, tweak)
+  result = result.toString('hex')
   time = Date.now() - time
-  if (result !== privateKeyTweakAdd) {
-    log('FAILED secp256k1.privateKeyTweakAdd 3')
+  if (result !== privateKeyTweakAddHex) {
+    log('FAILED ' + testName)
     return
   }
-  log('PASSED secp256k1.privateKeyTweakAdd 3. Time:' + time.toString())
+  log('PASSED ' + testName + '. Time:' + time.toString())
 
+  testName = 'secp256k1.privateKeyTweakAdd 4'
   privateKeyHex = '877c779ad9687164e9c2f4f0f4ff0340814392330693ce95a58fe18fd52e6e93'
   tweakHex = 'e8ce665a6b37fcf9fafbae7fa34738e19aa8edd3c98ad5f687eb3d4e89257765'
-  privateKeyTweakAdd = '704addf544a06e5ee4bea37098463c23613da32020d604506da8c0518e1da4b7'
+  privateKeyTweakAddHex = '704addf544a06e5ee4bea37098463c23613da32020d604506da8c0518e1da4b7'
   time = Date.now()
-  result = await crypto.secp256k1.privateKeyTweakAdd(privateKeyHex, tweakHex)
+  privateKey = Buffer.from(privateKeyHex, 'hex')
+  tweak = Buffer.from(tweakHex, 'hex')
+  result = await crypto.secp256k1.privateKeyTweakAdd(privateKey, tweak)
+  result = result.toString('hex')
   time = Date.now() - time
-  if (result !== privateKeyTweakAdd) {
-    log('FAILED secp256k1.privateKeyTweakAdd 4')
+  if (result !== privateKeyTweakAddHex) {
+    log('FAILED ' + testName)
     return
   }
-  log('PASSED secp256k1.privateKeyTweakAdd 4. Time:' + time.toString())
+  log('PASSED ' + testName + '. Time:' + time.toString())
 
   log('PASSED secp256k1.privateKeyTweakAdd ALL')
 }
@@ -234,11 +288,14 @@ async function testPrivateKeyTweakAdd (log) {
 async function testPublicKeyTweakAdd (log) {
   const publicKeyHex = '02fce1b65c7470270fb89e5aa1dda253406b3370b948eae8a2a3539c4f25894283'
   const tweakHex = '5e7ab5c14484291796c82b7ffd7ae72bc6a57cc53cc0d3b4312ddcf050c4a8af'
-  const publicKeyTweakAdd = '02e7eddf4cfa267c3b33a7e9387f28c55b6e806a79824c797b0f5acdfcdccc73e8'
+  const publicKeyTweakAddHex = '02e7eddf4cfa267c3b33a7e9387f28c55b6e806a79824c797b0f5acdfcdccc73e8'
+  const publicKey = Buffer.from(publicKeyHex, 'hex')
+  const tweak = Buffer.from(tweakHex, 'hex')
   let time = Date.now()
-  const result = await crypto.secp256k1.publicKeyTweakAdd(publicKeyHex, tweakHex, true)
+  let result = await crypto.secp256k1.publicKeyTweakAdd(publicKey, tweak, true)
+  result = result.toString('hex')
   time = Date.now() - time
-  if (result !== publicKeyTweakAdd) {
+  if (result !== publicKeyTweakAddHex) {
     log('FAILED secp256k1.publicKeyTweakAdd 1')
     return
   }
